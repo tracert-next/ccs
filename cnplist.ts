0c926ea499fb2c3c39c031c378ee362ad4a15ff3
tracert-next <153575600+tracert-next@users.noreply.github.com>
2025-04-14 12:16:47 +0900

    Create cnplist.ts

---
+++ cnplist.ts
@@ -0,0 +1,21 @@
+{
+    "name": "CNPPlatformList", 
+    "logoURI": "http://www.cnpfound.com/cnp/CCS.png", 
+    "timestamp": 1610339529000, 
+    "tokens": [
+        {
+            "chainId": 1, 
+            "address": "TKKThcehVYQdhY6hoevZQqfPvo8tcbQmZh", 
+            "name": "CCSCoinContractVer01",
+            "symbol": "CCS",
+            "decimals": 18, 
+            "logoURI": "http://www.cnpfound.com/cnp/CCS.png" 
+            }
+    ],
+    "version": 
+        { 
+            "major": 1, 
+            "minor": 0, 
+            "patch": 0 
+        }
+   }
